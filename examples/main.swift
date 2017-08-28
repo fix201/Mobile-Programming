83f20214ee4149d572ccc990c00fcca43959dec8
Olufisayo Ayodele <oayodele1@ucamath-26.local>
2017-08-28 15:20:08 -0500

    Initial Commit

---
+++ examples/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  examples
+//
+//  Created by Olufisayo Ayodele on 8/28/17.
+//  Copyright © 2017 Olufisayo Ayodele. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
